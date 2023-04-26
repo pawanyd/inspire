1d052bbff139d2ac4ebb29a2cb61ac8253f516f3
pawan kumar <pawanyd2012@gmail.com>
2023-04-26 15:56:14 +0530

    test

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -8,7 +8,7 @@
     <title>Inspire</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
-
+<!-- this is test body -->
 <body>
     <div class="container">
         <h1>{{ $quote }}</h1>
